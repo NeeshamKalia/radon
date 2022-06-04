a2e5361d82226fd2dd71e0711843b66a736c2d14
Neesham Kalia <neeshamkalia007@gmail.com>
2022-06-04 08:53:57 +0530

    missing no. in array

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+router.get('/sol1', function (req, res) {
+    let arr = [1,2,3,5,6,7]
+    let missingNumber;
+    // sum of n numbers is n(n+2)/2
+    sumN = ((arr.length+1) * (arr.length+2))/2;
+    sumArr = 0;
+    for(let i=0; i<arr.length; i++){
+        sumArr +=  arr[i];
+    } 
+    missingNumber = sumN - sumArr;
+    res.send({data: missingNumber});
+});
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+router.get('/sol2',function(req,res){
+    let arr = [33,34,35,37,38]
+    let missingNumber;
+    sumN = arr.length*((33+38)/2)
+    
+    
+    sumN = (arr.length+1)*((33+38)/2)
+    sumArr = 0;
+    for ( let i=0; i<arr.length; i++){
+        sumArr += arr[i]
+    }
+    missingNumber = sumN - sumArr
 
+    res.send({data: missingNumber});
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+});
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+
+
+
 
 module.exports = router;
