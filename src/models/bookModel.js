aca97ae0d06f03372eaf1bb0f5f171314b5529d9
Neesham Kalia <neeshamkalia007@gmail.com>
2022-06-05 19:32:32 +0530

    mongodb 1 assignment solution

---
+++ src/models/bookModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+
+const bookSchema = new mongoose.Schema({
+    bookName: {
+              type: String,
+              required: true
+            },
+    AutherName: String,
+    category: {
+                type: String,
+                enum: ["nonFiction", "fiction", "Biography", "youngAdult"],
+                },
+
+    year: Number,
+    
+});
+          
+        
+ 
+
+
+module.exports = mongoose.model('Book', bookSchema) 
+
+
+
+/* onst userSchema = new mongoose.Schema( {
+    
+    firstName: String,
+    lastName: String,
+    mobile: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    emailId: String,
+    gender: {
+        type: String,
+        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    },
+    age: Number,
+    // isIndian: Boolean,
+    // parentsInfo: {
+    //     motherName: String,
+    //     fatherName: String,
+    //     siblingName: String
+    // },
+    // cars: [ String  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema) //users */
+
+
+
+// String, Number
+// Boolean, Object/json, array
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
+const bookModel= require("../models/bookModel.js")
 const UserController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", async function(req,res){
+    let data = req.body
+    let savedBook = await bookModel.create(data)
+    res.send({msg: savedBook})
+}  );
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/bookList",  async function(req,res){       //UserController.getUsersData
+    let bookList  = await bookModel.find()
+    res.send({msg: bookList})
+});            
 
 module.exports = router;
